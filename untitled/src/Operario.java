cac3310c8e4d0fd2905749668fa07f4371f0b287
Lorenzoancioto <161514581+Lorenzoancioto@users.noreply.github.com>
2024-11-08 22:02:41 -0300

    atividade(SOLID)
    O nome do arquivo bugou e ficou como indefinido!!

---
+++ untitled/src/Operario.java
@@ -0,0 +1,6 @@
+public class Operario implements FuncionarioOperacional{
+    @Override
+    public void realizarAtividade(){
+        System.out.println("Realizando atividade operacional...");
+    }
+}
